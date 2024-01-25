6143709164bad6e10eff07def1002a9e3f72c31d
hathanhdung <hathanhdung1995@gmail.com>
2024-01-25 23:46:36 +0700

    Oh no

--- src/DemoCICD.API/Middleware/ExceptionHandlingMiddleware.cs
+++ src/DemoCICD.API/Middleware/ExceptionHandlingMiddleware.cs
@@ -14,6 +14,7 @@ namespace DemoCICD.API.Middleware
         {
             try
             {
+                //Shit
                 await next(context);
             }
             catch (Exception e)
